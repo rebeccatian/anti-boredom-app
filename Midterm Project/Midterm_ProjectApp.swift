e6c8c5d4b35ce7f59b65bc6e0b49607c154db488
Rebecca Tian <re>
2022-10-17 01:08:15 -0400

    Initial Commit

---
+++ Midterm Project/Midterm_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Midterm_ProjectApp.swift
+//  Midterm Project
+//
+//  Created by rtd2gu on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Midterm_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
